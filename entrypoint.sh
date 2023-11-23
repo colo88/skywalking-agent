bc0122116e6a983b4a0276450142d4f15e66698d
Qi Zhang <13651281168@163.com>
2023-11-23 18:19:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [ -z "$(ls -A /tmp/)" ]; then
-  echo "Copy skywalking agent file to tmp directory"
+if [ -z "$(ls -A /tmp/agent)" ]; then
+  echo "Copying skywalking agent file to tmp/agent directory·····················"
   cp -vR /skywalking/agent /tmp/
+  echo "Copy skywalking agent file to tmp/agent directory Successful····················"
 else
-  echo "directory tmp not empty"
+  echo "Directory tmp/agent file already exists····················"
 fi
